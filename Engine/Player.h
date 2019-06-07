66ed5f927f9f7456a1e07df43cb5c9261ff93a68
SunTroll <50963945+Sun-Troll@users.noreply.github.com>
2019-06-07 23:45:23 +0200

    balance

--- Engine/Player.h
+++ Engine/Player.h
@@ -10,13 +10,13 @@ public:
 	void ClampScreen();
 	void Draw(Graphics& gfx) const;
 private:
-	static constexpr float acc = 15.0f;
+	static constexpr float acc = 14.0f;
 	static constexpr float drag = 0.1f;
 	static constexpr float bounce = -0.6f;
 	float jumpVal = 0.0f;
 	static constexpr float jumpCharge = 20.0f;
 	static constexpr float jumpMax = 40.0f;
-	static constexpr float gravity = 10.0f;
+	static constexpr float gravity = 20.0f;
 	Color c{ 255, 255, 255 };
 	static constexpr int width = 40;
 	static constexpr int height = 40;
